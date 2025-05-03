ae1749327405c53eb9ee30d9b999385f730af632
henrique pinheiro <henrique.p.freire@hotmail.com>
2025-05-03 03:05:20 -0300

    feat: alter background image

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,10 +4,15 @@ import NavBar from "./components/NavBar"; // ajuste o caminho conforme necessár
 const Layout = () => (
     <>
         <NavBar />
-        <main className="pt-20" 
+        <main
+            className="pt-20"
             style={{
-                        backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('/images/background-image.png')`
-                    }}>
+                backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)), url('/images/background-image.png')`,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',           // faz a imagem cobrir toda a área
+                backgroundPosition: 'center'       // centraliza a imagem
+            }}
+        >
             <Outlet />
         </main>
     </>
